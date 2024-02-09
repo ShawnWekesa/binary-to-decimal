fa818e3c460ad0d80c08df886aed134d96b8b5b6
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-02-10 01:11:36 +0300

    binary to decimal complete

--- binary-to-decimal/index.js
+++ binary-to-decimal/index.js
@@ -1,2 +1,15 @@
 console.log('hi')
 
+let targetBinary = 1111101110;
+const temp = String(targetBinary).split('')
+let decimalValue = 0
+let exponent = 0
+
+for(i = temp.length - 1; i >= 0; i--){
+	if(temp[i] == 1){
+		decimalValue += (2**exponent)
+	}
+	exponent++
+}
+
+console.log(decimalValue)
